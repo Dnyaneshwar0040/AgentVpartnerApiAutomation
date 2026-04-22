84d3b79908b3cc3dc1d835c48f60d306078fb3fb
Dnyaneshwar0040 <dnyaneshwar.tahakik@indiconnect.in>
2026-04-22 17:23:46 +0530

    Initial Changes

--- src/test/java/AuthAPI/LoginPagePartnerAPI.java
+++ src/test/java/AuthAPI/LoginPagePartnerAPI.java
@@ -14,5 +14,6 @@ public class LoginPagePartnerAPI {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
